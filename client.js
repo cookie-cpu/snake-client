9fa4e90d06380cbb842add81033923e768f9db12
Matthew Cook <matthewcook13987@gmail.com>
2021-02-10 21:17:52 +0000

    Tinkered with code to test passing data functionality and moving the snake on the server via client inputs.

--- client.js
+++ client.js
@@ -12,8 +12,15 @@ const connect = function() {
   conn.on("connect", () => {
     console.log("Connection established. Connected to server")
   });
+  
   conn.on('connect', () => {
     conn.write('Name: MSC');
+  });
+
+  conn.on('connect', () => {
+    //setInterval(function(){conn.write("Move: up");}, 50)
+    //setInterval((conn.write('Move: up')),50)
+    //conn.write('Move: up');
   });
   
   return conn;

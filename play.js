6e43f01a68154f3b8ab73472429d8f7200dbe092
Matthew Cook <matthewcook13987@gmail.com>
2021-02-10 20:58:52 +0000

    created client.js and moved the connect function to that file leaving play.js with less and cleaner code.

--- play.js
+++ play.js
@@ -1,19 +1,7 @@
-const net = require('net');
+//const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function(data) {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  console.log("Dead because idle");
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
+const { connect } = require('./client.js')
 
 console.log('Connecting ...');
+
 connect();
